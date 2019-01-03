828e80455dca2ba10c507e1a9e6cfc4a04c8f458
Yiyang <hukahoooka@gmail.com>
2019-01-03 22:01:19 +0800

    [feature] board

--- cloud.py
+++ cloud.py
@@ -2,37 +2,41 @@ from creadcials import LEAN_APPKEY, LEAN_APPSEC
 import leancloud
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
-
-
+# logging.basicConfig(level=logging.DEBUG)
 leancloud.init(LEAN_APPKEY, LEAN_APPSEC)
 
-print(LEAN_APPKEY)
-print(LEAN_APPSEC)
-
 leancloud.use_region('US')
+
 
 def check_feedback(chat_id, message_id, user_id, data):
     data = 1 if data == '1' else 0
     emoji = ['😶', '❤️']
-    query_string = "select * from Message where message_id={} and chat_id={} limit 1".format(message_id, chat_id)
+    query_string = "select * from Message where message_id={} and chat_id={} limit 1".format(
+        message_id, chat_id)
     results = leancloud.Query.do_cloud_query(query_string).results
     text = 'You {} this message~'.format(emoji[data])
     if len(results):
         message = results[0]
         feedback = message.get('feedback')
+        contrib = 0
         if user_id not in feedback[data]:
             feedback[data].append(user_id)
+            contrib += 1
             if user_id in feedback[~data]:
                 feedback[~data].remove(user_id)
+                log_contributions(user_id, -1, "feedback")
+                contrib -= 1
         else:
             feedback[data].remove(user_id)
             text = 'You took back your {}~'.format(emoji[data])
+            contrib -= 1
+        log_contributions(user_id, contrib, "feedback")
         message.set('feedback', feedback)
         message.save()
     else:
         feedback = [[], []]
         feedback[data].append(user_id)
+        log_contributions(user_id, 1, "feedback")
         Message = leancloud.Object.extend('Message')
         message = Message()
         message.set('chat_id', chat_id)
@@ -41,8 +45,10 @@ def check_feedback(chat_id, message_id, user_id, data):
         message.save()
     return text
 
+
 def get_feedback(chat_id, message_id):
-    query_string = "select * from Message where message_id={} and chat_id={} limit 1".format(message_id, chat_id)
+    query_string = "select * from Message where message_id={} and chat_id={} limit 1".format(
+        message_id, chat_id)
     results = leancloud.Query.do_cloud_query(query_string).results
     likes = dislikes = 0
     if len(results):
@@ -50,3 +56,42 @@ def get_feedback(chat_id, message_id):
         likes = len(feedback[1])
         dislikes = len(feedback[0])
     return likes, dislikes
+
+
+def log_contributions(user_id, value, TYPE="feedback"):
+    weight = {
+        'feedback': 1,
+        'content': 5
+    }
+    user = get_user(user_id)
+    contrib = user.get('user_contrib')
+    user.set('user_contrib', value * weight[TYPE] + contrib)
+    user.save()
+
+def add_user(user_id, user_name, user_nick,):
+    user = get_user(user_id)
+    if (user):
+        return user
+    else:
+        User = leancloud.Object.extend('Users')
+        user = User()
+        user.set('user_id', user_id)
+        user.set('user_nick', user_nick)
+        user.set('user_name', user_name)
+        user.set('user_contrib', 0.0)
+        user.save()
+        return user
+
+
+def get_user(user_id):
+    query_string = "select * from Users where user_id={} limit 1".format(
+        user_id)
+    results = leancloud.Query.do_cloud_query(query_string).results
+    if (len(results)):
+        return results[0]
+    else:
+        return None
+
+def get_users():
+    query_string = "select * from Users limit 10"
+    return leancloud.Query.do_cloud_query(query_string).results
--- commands.py
+++ commands.py
@@ -1,9 +1,23 @@
-from telegram import Bot, Update, ChatAction, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Bot, Update, ChatAction, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from creadcials import *
 from utils import build_menu, like_buttons
+from cloud import get_users
 
 def start(bot: Bot, update: Update):
     chat_id = update['message']['chat_id']
     bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
     bot.send_message(chat_id=chat_id, text="本子社的机器人~")
     bot.send_message(chat_id=chat_id, text="向我发送一张图片，我会附上两个反馈按钮发送到附带的 Channel，并且不会显示是谁发的。不过如果有谁做坏事，我就把他拉黑！")
+
+def contributors(bot: Bot, update: Update):
+    chat_id = update['message']['chat_id']
+    if (chat_id != GROUP):
+        bot.send_message(chat_id=chat_id, text="该命令只能在本子社的 TG 群内部使用。")
+    else:
+        text = '*贡献榜 Top 10* \n'
+        i = 0
+        for user in get_users():
+            i += 1
+            text += "{} - {} - Points: {}\n".format(i, user.get('user_nick'), user.get('user_contrib'))
+        text += '----------\n _Tips: 发布图片，或者对图片进行反馈都可以增加点数！_'
+        bot.send_message(chat_id=chat_id, text=text, parse_mode=ParseMode.MARKDOWN)
