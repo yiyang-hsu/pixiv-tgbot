55961362b7e678e89427501d22e4b0249a64ca4f
Yiyang <yyhsu@protonmail.com>
2019-01-15 13:44:58 +0800

    commands

--- commands.py
+++ commands.py
@@ -8,6 +8,7 @@ def start(bot: Bot, update: Update):
     bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
     bot.send_message(chat_id=chat_id, text="本子社的机器人~")
     bot.send_message(chat_id=chat_id, text="向我发送一张图片，我会附上两个反馈按钮发送到附带的 Channel，并且不会显示是谁发的。不过如果有谁做坏事，我就把他拉黑！")
+    bot.send_message(chat_id=chat_id, text="现在支持直接分享给我来自 Pixiv 的链接来发送多张图片，但是暂不支持动图。")
 
 def contributors(bot: Bot, update: Update):
     chat_id = update['message']['chat_id']
